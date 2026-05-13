6007fe6e46638ec8f4d2f01752e6fd21986605a0
davidsaavedracastro09-beep <davidsaavedracastro09@gmail.com>
2026-05-12 19:16:14 -0500

    Menu princimal del programa

---
+++ main.py
@@ -0,0 +1,18 @@
+print("Hola, bienvenidos a mi programa que va simular tu banco de confianza")
+# Saldo inicial
+saldo = 1000
+
+# Lista para guardar operaciones
+historial = []
+
+# Variable para controlar el menú
+opcion = 0
+
+# Menú interactivo
+while opcion != 4:
+
+    print("CAJERO AUTOMÁTICO")
+    print("1. Consultar saldo")
+    print("2. Depositar dinero")
+    print("3. Retirar dinero")
+    print("4. Salir")
